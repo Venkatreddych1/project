d5b653a8697a64f321a24c6c2c4644deae260212
venkat <chereddyvenkatareddy@gmail.com>
2020-08-01 16:51:22 +0530

    fourth time

--- Empapp/tests/test_views.py
+++ Empapp/tests/test_views.py
@@ -1,19 +1,29 @@
 import requests
 import json
+import pytest
 
 
 
-class test_view():
-  def test_post_headers_body_json(self):
-      url = 'http://127.0.0.1:8000/'
 
-      # Body
-      payload = {"empid": 405, "name": "rajesh","emp_num": 1796,"department": "IT"}
+def test_post_headers_body_json(self):
+    url = 'http://127.0.0.1:8000/'
+    # Body
+    payload = {"empid": 4005, "name": "rajesh", "emp_num": 14126, "department": "IT"}
 
-      # convert dict to json by json.dumps() for body data.
-      resp = requests.post(url,data=json.dumps(payload))
+    # convert dict to json by json.dumps() for body data.
+    resp = requests.post(url, data=json.dumps(payload))
 
-      # Validate response headers and body contents, e.g. status code.
-      assert resp.status_code == 201
+    # Validate response headers and body contents, e.g. status code.
+    assert resp.status_code == 201
 
-      # print response full body as text
+    # print response full body as text
+
+
+
+
+def test_post_get_details(self):
+    url = 'http://127.0.0.1:8000/api/employee/'
+
+    resp=requests.get(url)
+    k = resp.json()
+    assert k.status_code == 200
